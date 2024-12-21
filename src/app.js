d20971917710511b80466e44e5cd09a8a0c8b147
Ritesh Kumar <ritesh02072002@gmail.com>
2024-12-21 17:45:51 +0530

    Crud Api

--- src/app.js
+++ src/app.js
@@ -4,15 +4,18 @@ const User = require('./models/user');
 
 const app = express();
 
-app.post('/signup',async (req,res)=>{
-    const userObj={
-        firstName:"Ritesh",
-        lastName:"Kumar",
-        emailId:"riteshk@gmail.com",
-        password:"12345"
-    }
+app.use(express.json());
 
-    const user = new User(userObj);
+app.post('/signup',async (req,res)=>{
+    console.log(req.body);
+    // const userObj={
+    //     firstName:"Ritesh",
+    //     lastName:"Kumar",
+    //     emailId:"riteshk@gmail.com",
+    //     password:"12345"
+    // }
+
+    const user = new User(req.body);
 
     try{
         await user.save();
@@ -23,7 +26,49 @@ app.post('/signup',async (req,res)=>{
     
 })
 
+// Feed API - GET /feed - get all the users for the database
 
+app.get('/feed',async (req,res)=>{
+
+    try{
+        const users=await User.find({});
+        res.send(users);
+    }catch(err)
+    {
+        res.status(400).send("something went wrong");
+    }
+})
+
+
+// Delete a user from the database
+app.delete('/user',async (req,res)=>{
+    const userId = req.body.userId;
+
+    try{
+        
+        const user=await User.findByIdAndDelete(userId);
+        res.send("user deleted successfully");
+    }catch(err)
+    {
+        res.status(400).send("something went wrong");
+    }
+})
+
+// Update data of a user 
+
+app.patch('/user',async(req,res)=>{
+
+    const userId=req.body.userId;
+    const data=req.body;
+
+    try{
+        await User.findByIdAndUpdate({_id:userId},data);
+        res.send("User updated successfully");
+    }catch(err)
+    {
+        res.status(400).send("something went wrong");
+    }
+})
 
 
 app.listen(3000,()=>{
