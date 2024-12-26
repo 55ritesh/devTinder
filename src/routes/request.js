da2e3cd7a84e0044fd8712e94fe3334a15183f5a
Ritesh Kumar <ritesh02072002@gmail.com>
2024-12-26 17:09:31 +0530

    logicalDb_query

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,16 +1,74 @@
 const express = require('express');
 const {userAuth} = require('../middlewares/auth');
+const ConnectionRequestModel = require('../models/connectionRequest');
+const User = require('../models/user');
 
 const requestRouter = express.Router();
 
 
-requestRouter.post("/sendConnectionRequest",userAuth,(req,res)=>{
+requestRouter.post("/request/send/:status/:toUserId",userAuth,async(req,res)=>{
 
-    const user=req.user;
-    //sending a connection request
-    console.log("sending a connection request");
+    try{
 
-    res.send(user.firstName+" sent the connection request");
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
+
+        const allowedStatus = ["ignored","interested"];
+
+        if(!allowedStatus.includes(status)){
+            return res.status(400).json({
+                message:"Invalid status type "+status
+            })
+        }
+
+
+        const existingConnectionRequest = await ConnectionRequestModel.findOne({
+            $or:[
+                {fromUserId,toUserId},
+                {fromUserId:toUserId, toUserId:fromUserId}
+            ],
+        });
+
+        const toUser = await User.findById(toUserId);
+        if(!toUser){
+            return res.status(400).json({message:"Connection Request already exist!!"});
+        }
+
+
+        if(existingConnectionRequest){
+            return res.status(400).json({message:"User not found.."});
+        }
+
+        const connectionRequestModel = new ConnectionRequestModel({
+            fromUserId,toUserId,status,
+        });
+
+        const data = await connectionRequestModel.save();
+
+        res.json({
+            message:req.user.firstName+" is "+status + " by "+toUser.firstName,
+            data,
+        })
+    }catch(err){
+        res.status(400).send("ERROR: "+err.message);
+    }
+
+  //  res.send(User.firstName+" sent the connection request");
+  
+})
+
+
+
+requestRouter.post("/request/review/:status/:requestId",userAuth,async (req,res)=>{
+
+    try{
+         
+        const loggedInUser = req.user;
+        
+    }catch(err){
+        res.status(400).send("ERROR: "+err.message);
+    }
 })
 
 
